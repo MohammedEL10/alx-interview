a35ad7e30123172d0ee90e4b32a44654b0e80cb5
MohammedEL10 <maohammedelhathout14@gmail.com>
2024-08-25 12:22:14 +0100

    0x08-making_change

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,16 +1,26 @@
+
 #!/usr/bin/python3
-def makeChange(coins, total):  
-    if total <= 0:  
-        return 0  
-    
-    dp = [float('inf')] * (total + 1)  
-    dp[0] = 0  # Base case: 0 coins are needed to make total of 0  
-    
-    # Iterate over each coin  
-    for coin in coins:  
-        # For each coin, update the dp array from the coin value to total  
-        for t in range(coin, total + 1):  
-            dp[t] = min(dp[t], dp[t - coin] + 1)  
-    
-    # If dp[total] is still infinity, return -1 (not possible to form total)  
-    return dp[total] if dp[total] != float('inf') else -1
+""" Making changes """
+
+
+def makeChange(coins, total):
+    """ Generate changes needed to reach total
+
+    Args:
+        coins ([List]): [List of Coins available]
+        total ([int]): [total amount needed]
+    """
+    if total <= 0:
+        return 0
+    check = 0
+    temp = 0
+    coins.sort(reverse=True)
+    for i in coins:
+        while check < total:
+            check += i
+            temp += 1
+        if check == total:
+            return temp
+        check -= i
+        temp -= 1
+    return -1
